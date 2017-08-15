2e1d634a82fce47a9688acb27cd5ce1f757c184a
Hyman <1604723751@qq.com>
2017-08-15 09:54:25 +0800

    add facade pattern structure

---
+++ FacadePattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacadePattern.Structure;
+
+namespace FacadePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+            facade.MethodA();
+            facade.MethodB();
+            Console.Read();
+        }
+    }
+}
